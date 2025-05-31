dd213b88cb5e9174e5af376f86d5d16e06db450c
Abhijeet123690 <Abhijeetkumar123690@gmail.com>
2025-05-31 15:59:41 +0530

    Update daily.js

--- pages/api/pages/api/pages/api/daily.js
+++ pages/api/pages/api/pages/api/daily.js
@@ -1,9 +1,8 @@
-// ✅ money-matrix/pages/api/search.js
 export default async function handler(req, res) {
-  const { keywords } = req.query;
+  const { symbol } = req.query;
   const apiKey = process.env.ALPHA_VANTAGE_API_KEY;
 
-  const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keywords}&apikey=${apiKey}`;
+  const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&outputsize=compact&apikey=${apiKey}`;
   const response = await fetch(url);
   const data = await response.json();
 
